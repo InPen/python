4282bb227a9f5d6af14115cb798b77635384c399
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-10-07 17:45:22 -0400

    Testing what I've learned thus far

---
+++ quiz1/ problem1.md
@@ -0,0 +1,19 @@
+# Author: Maria Peniche
+
+Mini Quiz
+
+1. True or false: An if statement can live without an else statement.
+> True
+2. True or false: An else statement can live without an if statement.
+> False
+3. True or false: An elif statement can live without an if statement.
+> False
+4. True or false: An if + elif statement can live without an else statement.
+> True
+5. True or false: The following statements are equivalent. Hint: Think about Truthyness and equality!
+https://alpastor.herokuapp.com/unit-2-control-flow/instructor-resources/hw-10wk-class-2/README.pdf
+
+> False, == checks for complete equality
+
+6. False
+7. 2
---
+++ quiz1/problem2.py
@@ -0,0 +1,23 @@
+# Author: Maria Peniche
+
+# create a variable, time, which has an integer value between zero and 23
+time = 8
+# If the time of day is before 9 a.m.
+if time > 9
+    print("Good morning, time to wake up and smell the coffee!")
+
+# if the time of day is before or exactly 4 p.m.
+elif time >= 16
+    print("Work work work... when will it end?")
+
+# if the time of day is before 8 p.m.
+elif time > 20
+    print("How did it get so late so soon?")
+
+# if the time of day is before 10 p.m.
+elif time > 22
+    print("A day without sunshine is like, you know, night.")
+
+# else for any time 10 p.m. or later
+else time <= 22
+    print("Burning the midnight oil!")
