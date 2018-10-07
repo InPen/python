90bb72d2288730e96c7828cf895b84754acc116f
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-10-07 17:46:16 -0400

    Optamizing the frist code I built to generate movie titles and ratings that requires user input

---
+++ movieApp/movie_app.py
@@ -0,0 +1,47 @@
+#Author: Maria Peniche
+
+# Unit 1
+
+movie_titles = ['Aaladin', 'Hercules', 'ALL Harry Potter Movies', 'Frida']
+movie_ratings = 8
+# print("The movie", movie_title, "is ", movie_ratings)
+# print("The movie " + str(movie_title) + " is " + str(movie_ratings))
+
+# Unit 2
+
+def print_rating(movie_title):
+    print('The rating for', movie_title, 'is', movie_ratings)
+
+
+# this is the main function that will be invoked at the end of the file
+# def main():
+
+
+    # mode
+
+    # If mode is equal to search, your app will
+    # then print ('Back To The Future', 'Blade', 'Spirited Away')
+
+    # If mode is equal to ratings, your app will
+    # then print('The rating for Back To The Future is 8.')
+    # pass
+    # print('hello')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dunder main
+# if __name__ == "__main__":
+#  main()
