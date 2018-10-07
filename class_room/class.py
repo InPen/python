660a45d9040e4afd6476bfaa9290e7cb88b83198
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-10-07 17:41:19 -0400

    List === arrays

---
+++ class_room/class.py
@@ -0,0 +1,15 @@
+# Author: Maria Peniche
+
+# 1. Declare a list with the names of your classmates
+classmates = ['juanita', 'gerardo', 'pedro', 'ana', 'josefina']
+print(classmates)
+# 2. Print out the length of that list
+len(classmates)
+# 3. Print the 3rd name on the list
+print(classmates[2])
+# 4. Delete the first name on the list
+classmates.pop(0)
+print(classmates)
+# 5. Re-add the name you deleted to the end of the list
+classmates.insert(0, "juanita")
+print(classmates)
