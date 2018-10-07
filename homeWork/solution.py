4cf45807b7298277075310fe4c494f506245fa0b
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-10-07 17:36:45 -0400

    Include first assignment Learn how to declare variables and print to the terminal

---
+++ homeWork/solution.py
@@ -0,0 +1,78 @@
+# Maria Peniche
+
+# 1 = "A"
+# 2 = "B"
+# 3 = "C"
+# 4 = "D"
+# 5 = "E"
+# 6 = "F"
+# 7 = "G"
+# 8 = "H"
+# 9 = "I"
+# 10 = "J"
+# 11 = "K"
+# 12 = "L"
+# 13 = "M"
+# 14 = "N"
+# 15 = "O"
+# 16 = "P"
+# 17 = "Q"
+# 18 = "R"
+# 19 = "S"
+# 20 = "T"
+# 21 = "U"
+# 22 = "V"
+# 23 = "W"
+# 24 = "X"
+# 25 = "Y"
+# 26 = "Z"
+
+beeps = 2
+boops = 6
+total = beeps + boops
+print(tota)
+
+beeps = 0
+boops = 5
+total = beeps + boops
+print(total)
+
+beeps = 9
+boops = 3
+total = beeps + boops
+print(total)
+
+beeps = 4
+boops = 8
+total = beeps + boops
+print(total)
+
+beeps = 10
+boops = 5
+total = beeps + boops
+print(total)
+
+beeps = 11
+boops = 12
+total = beeps + boops
+print(total)
+
+beeps = 5
+boops = 5
+total = beeps + boops
+print(total)
+
+beeps = 1
+boops = 17
+total = beeps + boops
+print(total)
+
+beeps = 5
+boops = 7
+total = beeps + boops
+print(total)
+
+beeps = 4
+boops = 0
+total = beeps + boops
+print(total)
